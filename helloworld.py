ae9eb7ebc7a1cbded61ff6ca0084eda9f01a5468
hothianhtam252002 <80697031+hothianhtam252002@users.noreply.github.com>
2023-12-27 22:50:45 +0700

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,18 @@
+#bai1
+'''print(input())'''
+#bai2
+'''nhap = 'Hello world'
+print(nhap)'''
+#bai3
+'''a = 'Hello'
+b = 'world'
+c = a+' '+b
+print(c)'''
+#bai4
+'''def print_to_screen(d):
+    return print(d)
+a = 'Hello'
+b = 'world'
+c = a+' '+b
+print_to_screen(c)'''
+
